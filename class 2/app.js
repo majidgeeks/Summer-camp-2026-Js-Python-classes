8c4c65ac0bc024a074168cb0ed4adb6b8b90b2df
Majid Hussain <majidrajput246@gmail.com>
2026-06-08 18:23:21 +0500

    two classes code

---
+++ class 2/app.js
@@ -0,0 +1,37 @@
+// console.log('hello')
+// console.log('world') 
+// console.log('dream')
+
+// console.log('Saylani')
+
+
+// alert('Hello world')
+
+// prompt('your name')
+
+// var myName //  daclaration
+
+// myName = 'Majid' // assign a value
+
+// console.log(myName)
+
+// var userName = 'Wasiq'
+
+
+// var myDream = 'lexus';
+
+// console.log('before', myDream)
+
+// var myDream = 'Toyota'
+
+// console.log('after',myDream)
+
+// let user1 = 'logo'
+
+// let user1 = 'logo3'
+
+// console.log(user1)
+
+
+
+
