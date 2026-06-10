4176c9c54c51a745b0465f342d73ea7dec1abb5e
Majid Hussain <majidrajput246@gmail.com>
2026-06-10 18:52:36 +0500

    python and js

---
+++ class 3/app.js
@@ -0,0 +1,73 @@
+// console.log('hello')
+// console.log('world') 
+// console.log('dream')
+
+// console.log('Saylani')
+
+
+// alert('Hello world')
+
+// prompt('your name')
+
+// var myName //  daclaration
+
+// myName = 'Majid' // assign a value
+
+// console.log(myName)
+
+// var userName = 'Wasiq'
+
+
+// var myDream = 'lexus';
+
+// console.log('before', myDream)
+
+// var myDream = 'Toyota'
+
+// console.log('after',myDream)
+
+// let user1 = 'logo'
+
+// let user1 = 'logo3'
+
+// console.log(user1)
+
+
+
+
+
+
+// comparison operator
+// > greater than
+
+// let num1 = 5
+
+
+// console.log(num1 > 7)
+
+// console.log(num1 < 4)
+
+
+// console.log(num1 < 8)
+
+// console.log(num1 >= 6)
+
+// &&  and 
+//  ||  or 
+
+
+//  + , -, *, /, %
+
+
+// IF ELSE
+
+
+
+
+
+
+
+
+
+
+
---
+++ class 3/p1.py
@@ -0,0 +1,19 @@
+# print('Hello world')
+
+# user = 'Majid'
+# roll = 1234
+# rollNum = 4567
+
+# print(user)
+# print(roll)
+# print(rollNum)
+
+# input('user name')
+
+# userName = input('Name : ')
+# fatherName = input('father Name : ')
+
+# print(userName, fatherName)
+
+# name = 'Mjid'
+
