09109357c5451817a51ae46bdab0b1e0ba9d8362
Majid Hussain <majidrajput246@gmail.com>
2026-06-12 18:21:49 +0500

    if else in js

---
+++ class 4/app.js
@@ -0,0 +1,80 @@
+
+
+
+
+
+
+
+// comparison operator
+// > greater than
+
+// let num1 = 5
+
+
+// console.log(num1 > 7)
+
+// console.log(num1 < 4)
+
+
+// console.log(num1 < 8)
+
+// console.log(num1 >= 6)
+
+// &&  and 
+//  ||  or 
+// ==
+// =
+// ===
+// == use for value
+// === user for value + data type
+
+
+let num1 = 10 // number
+let user = false // boolean
+let numeric = 20 // number
+let num2 = '10' // string
+
+// if(num1 < 9 || num2 == 10){
+//   alert('you are right')
+//   console.log('right')
+// }else{
+//   alert('you are wrong')
+// }
+
+
+// if(num1 < 9 && num2 == 10){
+//   alert('you are right')
+//   console.log('right')
+// }else{
+//   alert('you are wrong')
+// }
+
+// if(num1 > 9 && num2 === "10"){
+//   alert('you are right')
+//   console.log('right')
+// }else{
+//   alert('you are wrong')
+// }
+
+
+
+
+//  + , -, *, /, %
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
