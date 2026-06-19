5503e701cbe914ddce229fd5998753324e9067a6
Majid Hussain <majidrajput246@gmail.com>
2026-06-19 19:21:38 +0500

    class 7

---
+++ class 7/app.js
@@ -0,0 +1,54 @@
+// let user = false;
+
+// if(!user){
+//     alert('user not found');
+// }else{
+//     alert('user found');
+// };
+
+
+// let num1 = 10;
+// let num2 = '10';
+
+// let compare = num1 != num2;
+
+// let compare2 = num1 !== num2;
+
+// document.write(compare + '<br>')
+// document.write(compare2)
+
+
+// let module = 5 % 2 == 0;
+
+// document.write(module)
+
+
+// let userInput = prompt('enter number');
+
+// if(userInput % 2 === 0){
+//     document.write('Even number')
+// }else{
+//     document.write('Odd number')
+
+// }
+
+
+// let num1 = 11;
+
+// num1++;
+
+// document.write('before' + num1 + '<br>')
+
+// num1--;
+
+// document.write('after ', num1 + '<br>')
+
+// num1--;
+
+// document.write('an other after ', num1)
+
+
+// let num = 5;
+// let result = ++num;
+// console.log(num);
+// console.log(result);
