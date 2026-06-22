07954d8f09b2e7f9f03906674066adbac865abd7
Majid Hussain <majidrajput246@gmail.com>
2026-06-22 18:26:45 +0500

    class 8

---
+++ class 8/app.js
@@ -0,0 +1,43 @@
+// for(initialization, condition, increment/decrement)
+
+// for(let i = 1; i <= 10; i++){
+//     document.write(i + '<br>')
+// }
+
+
+
+
+// first  1
+//  second 2
+// third 3 
+//       4
+//       5
+//  6
+//  7 
+// 8
+// 9
+// 10
+
+
+// for(let i = 10; i >= 1; i--){
+//    document.write(i + '<br>')
+// }
+
+
+// for(let i = 1; i <= 11; i = i + 2){
+//    document.write(i + '<br>')
+
+// }
+
+
+// for(let i = 11; i >= 1; i = i - 2){
+//    document.write(i + '<br>')
+
+// }
+
+
+for(let i = 0; i <= 10; i++){
+//    document.write(i + ' : myName' + '<br>')
+  document.write(`${i} : saylani <br>`)
+
+}
