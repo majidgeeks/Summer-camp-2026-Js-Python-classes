eb91ea336ad5a95f412f1d4d7149b0a31f15816b
Majid Hussain <majidrajput246@gmail.com>
2026-07-01 18:00:34 +0500

    just class 11 dom

---
+++ class 11/app.js
@@ -0,0 +1,37 @@
+
+// let h1 = document.createElement('h1');
+// h1.innerHTML = 'Hello world'
+// console.log('h1',h1);
+
+// // console.log(document.body);
+
+// document.body.append(h1)
+
+// let p =  document.createElement('p');
+// p.innerHTML = ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus vero,
+//       officiis eius quisquam provident maiores vitae voluptatem, culpa aperiam
+//       similique ea. Optio dignissimos placeat ex quam architecto? Inventore,
+//       vero expedita?`
+
+// console.log('p',p);
+
+
+// let btn = document.createElement('button')
+// btn.innerText = 'Submit'
+// console.log('btn',btn);
+
+// let h3 = document.createElement('h3');
+// h3.innerHTML = 'Hello bhai';
+// console.log(h3);
+
+// document.body.append(h3)
+
+
+// creating heading
+let head = document.getElementById('header');
+head.innerHTML = 'Hello students'
+console.log(head);
+
+
+
+
